5cf33748ef73250eae91469621765115f10484aa
omikhe <msakamike84@gmail.com>
2025-12-07 17:44:38 +0200

    update days_to_units function to accept user input for number of days

--- workbook.py
+++ workbook.py
@@ -5,4 +5,4 @@ def days_to_units(num_of_days):
     print(f"{num_of_days} days are {num_of_days * calculation_to_hours } {name_of_unit}")
 
 
-days_to_units(40)
+days_to_units(int(input("Enter number of days: ")))
