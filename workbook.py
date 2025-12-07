04c03151f3a942d90e0c86b407a68081e6e0a8af
omikhe <msakamike84@gmail.com>
2025-12-07 19:46:38 +0200

    refactor days_to_units and validation logic for better clarity and structure

--- workbook.py
+++ workbook.py
@@ -2,18 +2,26 @@ calculation_to_hours = 24
 name_of_unit = "hours"
 
 def days_to_units(num_of_days):
-    if num_of_days > 0:
         return f"{num_of_days} days are {num_of_days * calculation_to_hours } {name_of_unit}"
-    elif num_of_days == 0:
-        return "0 days?, really?"
+    
+
+def validate_and_execute():
+
+    #check if num is a digit
+    if user_input.isdigit():
+        user_input_number = int(user_input)
+
+        #check if num is positive
+        if user_input_number > 0:
+            calculated_value = days_to_units(user_input_number)
+            print(calculated_value)
+        elif user_input_number == 0:
+            print("0 days?, really?")
+        else:
+            print("you entered a negative value")
+        
     else:
-        return "you entered a negative value"
+        print("that's not a number!")
 
 user_input = input("Enter number of days\n")
-
-if user_input.isdigit():
-    user_input_number = int(user_input)
-    data = days_to_units(user_input_number)
-    print(data)
-else:
-    print("that's not a number!")
+validate_and_execute()
