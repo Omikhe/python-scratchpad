06c28739ce94b38497af7143b6986006319f6a17
omikhe <msakamike84@gmail.com>
2025-12-07 18:26:19 +0200

    validate user input using conditionals

--- workbook.py
+++ workbook.py
@@ -2,7 +2,14 @@ calculation_to_hours = 24
 name_of_unit = "hours"
 
 def days_to_units(num_of_days):
-    print(f"{num_of_days} days are {num_of_days * calculation_to_hours } {name_of_unit}")
+    if num_of_days > 0:
+        return f"{num_of_days} days are {num_of_days * calculation_to_hours } {name_of_unit}"
+    elif num_of_days == 0:
+        return "0 days?, really?"
+    else:
+        return "you entered a negative value"
 
+user_input = int(input("Enter number of days\n"))
+data = days_to_units(user_input)
+print(data)
 
-days_to_units(int(input("Enter number of days: ")))
