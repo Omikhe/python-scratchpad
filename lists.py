3d672970fe6bcaffb6b1f92d3347be70ace71eef
omikhe <msakamike84@gmail.com>
2025-12-08 02:26:35 +0200

    add list and set examples with print statements

--- lists.py
+++ lists.py
@@ -1,7 +1,14 @@
 my_list = ["January", "February", "March", "April", "May", "June", "July"]
 
-print(my_list[9])
+print(my_list[0])
 
 my_list.append("August")
 
-print(my_list) 
+print(my_list)
+print(len(my_list))
+
+#creating a list using the list() constructor
+new_list = list(("CPU", "RAM", "Processor", "Storage"))
+
+for component in new_list:
+    print(component)
---
+++ sets.py
@@ -0,0 +1,14 @@
+my_set = {"January", "February", "March"}
+
+for month in my_set:
+    print(month)
+
+my_set.add("April")
+
+print(len(my_set))
+print(type(my_set))
+
+# creating a set using the set() constructor
+new_set = set(("apple", "mango", "pineapple"))
+for fruit in new_set:
+    print(fruit)
